e75c3a76fa1a9d01294a0d87c35fe3fa140da938
Murray-Smith24 <Murray.Smith@southwark.gov.uk>
2024-05-22 10:42:44 +0100

    added code

--- src/app.js
+++ src/app.js
@@ -3,11 +3,44 @@ const app = express();
 const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 //TODO: Create your GET Request Route Below: 
-app.get("/restaurants", async function(req,res) {
-    const restaurantsReturn = await Restaurant.findAll();
-    res.send(res.json(restaurantsReturn));
+
+
+//GET
+
+app.get("/restaurants", async (req,res) => {
+    const restaurants = await Restaurant.findAll();
+    res.json(restaurants);
 })
+
+app.get("/restaurants/:id", async function(req,res) {
+    const number = req.params.id;
+    const restaurants = await Restaurant.findByPk(number);
+    res.json(restaurants);
+});
+
+
+//PUT
+app.put("/restaurants", async (req,res) => {
+    res.json(await Restaurant.create(req));
+});
+
+
+//POST
+app.post("/restaurants/:id", async (req,res) => {
+    Restaurant[req.params.id] = req.body.Restaurant;
+    res.json({Restaurant});
+})
+
+//DELETE
+app.delete("/restaurants/:id", async (req,res) => {
+    await Restaurant.splice(req.params.id, 1);
+    res.json({Restaurant});
+})
+
 
 
 
